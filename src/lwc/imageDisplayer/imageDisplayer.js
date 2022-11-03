e7134e8d7e9aa41c0c274a105c4b671a4d5e2796
Yurii Bubis <yurii.bubis@avenga.com>
2022-11-03 10:49:42 +0200

    Creating Candidate with related Position Candidates list and related file but need to be refactored

---
+++ src/lwc/imageDisplayer/imageDisplayer.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yurii.bubis on 11/3/2022.
+ */
+
+import {LightningElement, wire, api} from 'lwc';
+import setImageUrl from '@salesforce/apex/ImageDisplayHelper.setImageUrl';
+export default class ImageDisplayer extends LightningElement {
+    @api recordId;
+    image;
+    // @wire (setImageUrl, {recordId: '$recordId'})
+    // image;
+    renderedCallback() {
+        setImageUrl({recordId: this.recordId})
+            .then(image => {
+                this.image = image
+                console.log(image);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+}
